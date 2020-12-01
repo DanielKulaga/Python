793ed6bb0b97c7b774f75c09f3f21957845d4e53
DanielKulaga <64287742+DanielKulaga@users.noreply.github.com>
2020-12-02 00:13:23 +0100

    Create Point.py

---
+++ Zestaw7/Circle/Point.py
@@ -0,0 +1,24 @@
+from Circle import *
+
+class Point:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+
+    def __repr__(self):
+        return "Point({}, {})".format(self.x, self.y)
+
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+    def __add__(self, other):
+        self.x = self.x + other.x
+        self.y = self.y + other.y
+        return Point(self.x, self.y)
